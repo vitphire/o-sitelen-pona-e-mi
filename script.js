d21497e4b6527cb2b85df0456e0e5101e5ddf635
Vitphire <mininjawasd@gmail.com>
2024-02-27 10:21:23 +0100

    Added code to create name letters

--- script.js
+++ script.js
@@ -1,14 +1,61 @@
-function validateInput() {
+function update_name_letters() {
+    const nameOutline = document.getElementById('name-outline');
+    const nameLetters = document.getElementById('name-container')
+        .querySelectorAll('.name-letter');
+    let nameOutlineText = '[';
+    for (let i = 0; i < nameLetters.length; i++) {
+        let letter = nameLetters[i];
+        let glyphEls = letter.querySelectorAll('.glyph');
+        let selected = parseInt(letter.style.getPropertyValue('--letter-selected'));
+        let glyph = glyphEls[selected +1];
+        nameOutlineText += glyph.textContent + ' ';
+    }
+    nameOutlineText += ']';
+    nameOutline.textContent = nameOutlineText;
+}
+
+function set_letters(letters) {
+    const nameContainer = document.getElementById('name-container');
+    let els = nameContainer.querySelectorAll('.name-letter');
+    els.forEach(el => {
+        el.remove();
+    });
+
+    for (let i = 0; i < letters.length; i++) {
+        const letter = letters[i];
+        // create element to hold the letter glyphs
+        let nameLetter = document.createElement('div');
+        for (let definition of definitions.entries()) {
+            const [glyph, _] = definition;
+            if (glyph.startsWith(letter)) {
+                let glyphElement = document.createElement('div');
+                glyphElement.classList.add('glyph');
+                glyphElement.textContent = glyph;
+                nameLetter.appendChild(glyphElement);
+            }
+        }
+        nameLetter.classList.add('name-letter');
+        nameLetter.style.setProperty('--letter-index', i.toString());
+        nameLetter.style.setProperty('--letter-color', letter_colors[i % letter_colors.length]);
+        nameLetter.style.setProperty('--letter-selected', '0');
+        nameContainer.appendChild(nameLetter);
+    }
+
+    update_name_letters();
+}
+
+function onInputChange() {
     const textBox = document.getElementById('name-text-box');
     const statusText = document.getElementById('status-text');
     const textBoxContainer = document.getElementById('name-input-container');
 
     textBox.size = Math.max(4, textBox.value.length);
-
-    if (validate_text(textBox.value)) {
+    const [valid, length] = validate_text(textBox.value);
+    if (valid) {
         statusText.textContent = 'Valid!';
         textBoxContainer.classList.add('valid');
         textBoxContainer.classList.remove('invalid');
+        set_letters(textBox.value.substring(length).toLowerCase());
     } else {
         statusText.textContent = 'Invalid!';
         textBoxContainer.classList.add('invalid');
@@ -67,20 +114,154 @@ function validate_text(text) {
     }
     let rest = text.substring(starting_glyph.length + 1);
     const regex = /^((^[aeiou]|[pksmnl][aeiou]|[jt][aeou]|w[aei])([mn](?![mn]))?)+$/;
-    return (rest.length > 0
-        && rest[0] === rest[0].toUpperCase()
-        && rest.substring(1) === rest.substring(1).toLowerCase()
-        && regex.test(rest.toLowerCase())
-    )
-}
-
-document.addEventListener('DOMContentLoaded', validateInput);
-
-function get_pastel_color() {
-    const colors = [
-        'A6374B', '6C3570', '4B4073',
-        '2D4F94', '0D688C', '3A9E7A',
-        'A4BF8A', 'BD9A5C', 'C26747',
+    return [rest.length > 0
+    && rest[0] === rest[0].toUpperCase()
+    && rest.substring(1) === rest.substring(1).toLowerCase()
+    && regex.test(rest.toLowerCase()),
+        starting_glyph.length + 1
     ]
-    return colors[Math.floor(Math.random() * colors.length)];
 }
+
+document.addEventListener('DOMContentLoaded', onInputChange);
+
+const letter_colors = [
+    '#A4BF8A', '#A6374B', '#0D688C',
+    '#4B4073', '#3A9E7A', '#C26747',
+    '#2D4F94', '#BD9A5C', '#6C3570',
+]
+
+const definitions = new Map(Object.entries({
+    "a": "(emphasis, emotion or confirmation)",
+    "akesi": "reptile, amphibian",
+    "ala": "no, not, zero; [~ ala ~] (used to form a yes-no question); nothing",
+    "alasa": "to hunt, forage, seek | ALT (pv.) try to, attempt",
+    "ale": "all; abundant, countless, bountiful, every, plentiful; abundance, everything, life, universe; one hundr",
+    "anpa": "bowing down, downward, humble, lowly, dependent | ALT bottom, lower part, under, below, floor, beneath",
+    "ante": "different, altered, changed, other",
+    "anu": "or | ALT choose, decide",
+    "awen": "enduring, kept, protected, safe, waiting, staying; (pv.) to continue to, to keep",
+    "e": "(before the direct object)",
+    "en": "(between multiple subjects)",
+    "epiku": "epic, cool, awesome, amazing",
+    "esun": "market, shop, fair, bazaar, business transaction",
+    "ijo": "thing, phenomenon, object, matter",
+    "ike": "bad, negative; non-essential, irrelevant | ALT complicated, complex",
+    "ilo": "tool, implement, machine, device",
+    "insa": "centre, content, inside, between; internal organ, stomach",
+    "jaki": "disgusting, obscene, sickly, toxic, unclean, unsanitary",
+    "jan": "human being, person, somebody",
+    "jelo": "yellow, yellowish",
+    "jo": "to have, carry, contain, hold",
+    "kala": "fish, marine animal, sea creature",
+    "kalama": "to produce a sound; recite, utter aloud",
+    "kama": "arriving, coming, future, summoned; (pv.) to become, manage to, succeed in",
+    "kasi": "plant, vegetation; herb, leaf",
+    "ken": "to be able to, be allowed to, can, may; possible",
+    "kepeken": "to use, with, by means of",
+    "kijetesantakalu": "superfamily, including raccoons, weasels, otters, skunks, and red pandas",
+    "kili": "fruit, vegetable, mushroom",
+    "kin": "{see a} | ALT indeed, too, also, as well",
+    "kipisi": "split, cut, slice, sever; sharp",
+    "kiwen": "hard object, metal, rock, stone",
+    "ko": "clay, clinging form, dough, semi-solid, paste, powder",
+    "kon": "air, breath; essence, spirit; hidden reality, unseen agent",
+    "kule": "colorful, pigmented, painted | ALT of or relating to the LGBT+ community",
+    "kulupu": "community, company, group, nation, society, tribe",
+    "kute": "ear; to hear, listen; pay attention to, obey",
+    "la": "(between the context phrase and the main sentence)",
+    "lanpan": "take, seize, catch, receive, get",
+    "lape": "sleeping, resting",
+    "laso": "blue, green",
+    "lawa": "head, mind; to control, direct, guide, lead, own, plan, regulate, rule",
+    "leko": "stairs, square, block, corner, cube",
+    "len": "cloth, clothing, fabric, textile; cover, layer of privacy",
+    "lete": "cold, cool; uncooked, raw",
+    "li": "(between any subject except mi alone or sina alone and its verb; also to introduce a new verb for the s",
+    "lili": "little, small, short; few; a bit; young",
+    "linja": "long and flexible thing; cord, hair, rope, thread, yarn | ALT line, connection",
+    "lipu": "flat object; book, document, card, paper, record, website",
+    "loje": "red, reddish",
+    "lon": "located at, present at, real, true, existing | ALT (affirmative response)",
+    "luka": "arm, hand, tactile organ; five | ALT touch/feel physically, interact, press",
+    "lukin": "eye; look at, see, examine, observe, read, watch; look for, seek; (pv.) try to",
+    "lupa": "door, hole, orifice, window",
+    "ma": "earth, land; outdoors, world; country, territory; soil",
+    "mama": "parent, ancestor; creator, originator; caretaker, sustainer",
+    "mani": "money, cash, savings, wealth; large domesticated animal",
+    "meli": "woman, female, feminine person; wife",
+    "meso": "midpoint, medium, mediocre; neither one not the other, neither fully is nor isn't",
+    "mi": "I, me, we, us",
+    "mije": "man, male, masculine person; husband",
+    "misikeke": "medicine, medical",
+    "moku": "to eat, drink, consume, swallow, ingest",
+    "moli": "dead, dying",
+    "monsi": "back, behind, rear",
+    "monsuta": "fear, dread; monster, predator; threat, danger",
+    "mu": "(animal noise or communication) | ALT (non-speech vocalization)",
+    "mun": "moon, night sky object, star | ALT glow, glowing light, light in the dark",
+    "musi": "artistic, entertaining, frivolous, playful, recreation",
+    "mute": "many, a lot, more, much, several, very; quantity | ALT three (or more), 20",
+    "n": "(indicates thinking, pondering, recognition, agreement, or humming)",
+    "namako": "{see sin} | ALT embellishment, spice; extra, additional",
+    "nanpa": "-th (ordinal number); numbers",
+    "nasa": "unusual, strange; silly; drunk, intoxicated",
+    "nasin": "way, custom, doctrine, method, path, road",
+    "nena": "bump, button, hill, mountain, nose, protuberance",
+    "ni": "that, this",
+    "nimi": "name, word",
+    "noka": "foot, leg, organ of locomotion; bottom, lower part",
+    "o": "hey! O! (vocative, imperative, or optative)",
+    "oko": "{see lukin} | ALT eye, ocular, visual {cf. lukin}",
+    "olin": "love, have compassion for, respect, show affection to",
+    "ona": "he, she, it, they",
+    "open": "begin, start; open; turn on",
+    "pakala": "botched, broken, damaged, harmed, messed up | ALT (curse expletive, e.g. fuck!)",
+    "pali": "do, take action on, work on; build, make, prepare",
+    "palisa": "long hard thing; branch, rod, stick",
+    "pan": "cereal, grain; barley, corn, oat, rice, wheat; bread, pasta",
+    "pana": "give, send, emit, provide, put, release",
+    "pi": "of (used to divide a second noun group that describes a first noun group) | ALT (introduces a genitive ",
+    "pilin": "heart (physical or emotional); feeling (an emotion, a direct experience)",
+    "pimeja": "black, dark, unlit",
+    "pini": "ago, completed, ended, finished, past",
+    "pipi": "bug, insect, ant, spider",
+    "poka": "hip, side; next to, nearby, vicinity | ALT along with (comitative), beside",
+    "poki": "container, bag, bowl, box, cup, cupboard, drawer, vessel",
+    "pona": "good, positive, useful; friendly, peaceful; simple",
+    "sama": "same, similar; each other; sibling, peer, fellow; as, like",
+    "seli": "fire; cooking element, chemical reaction, heat source",
+    "selo": "outer form, outer layer; bark, peel, shell, skin; boundary",
+    "seme": "what? which?",
+    "sewi": "area above, highest part, something elevated; awe-inspiring, divine, sacred, supernatural",
+    "sijelo": "body (of person or animal), physical state, torso",
+    "sike": "round or circular thing; ball, circle, cycle, sphere, wheel; of one year",
+    "sin": "new, fresh; additional, another, extra",
+    "sina": "you",
+    "sinpin": "face, foremost, front, wall",
+    "sitelen": "image, picture, representation, symbol, mark, writing",
+    "soko": "fungus, fungi",
+    "sona": "know, be skilled in, be wise about, have information on; (pv.) know how to",
+    "soweli": "animal, beast, land mammal",
+    "suli": "big, heavy, large, long, tall; important; adult",
+    "suno": "sun; light, brightness, glow, radiance, shine; light source",
+    "supa": "horizontal surface, thing to put or rest something on",
+    "suwi": "sweet, fragrant; cute, innocent, adorable",
+    "tan": "by, from, because of; origin, cause",
+    "taso": "but, however; only",
+    "tawa": "going to, toward; for; from the perspective of; moving | ALT (pv.) going to",
+    "telo": "water, liquid, fluid, wet substance; beverages",
+    "tenpo": "time, duration, moment, occasion, period, situation",
+    "toki": "communicate, say, speak, talk, use language, think; hello",
+    "tomo": "indoor space; building, home, house, room",
+    "tonsi": "non-binary, gender-non-conforming | ALT trans, non-cisgender",
+    "tu": "two | ALT separate, cut",
+    "unpa": "have sexual relations with",
+    "uta": "mouth, lips, oral cavity, jaw",
+    "utala": "battle, challenge, compete against, struggle against",
+    "walo": "white, whitish; light-coloured, pale",
+    "wan": "unique, united; one",
+    "waso": "bird, flying creature, winged animal",
+    "wawa": "strong, powerful; confident, sure; energetic, intense",
+    "weka": "absent, away, ignored",
+    "wile": "must, need, require, should, want, wish",
+}));
